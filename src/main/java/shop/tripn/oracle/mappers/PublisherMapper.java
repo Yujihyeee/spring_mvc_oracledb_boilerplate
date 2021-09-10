b9e33635fe7208f1c3965d3da09279b166c16baa
Yujihyeee <rladnddl@naver.com>
2021-09-10 16:07:32 +0900

    first commit

---
+++ src/main/java/shop/tripn/oracle/mappers/PublisherMapper.java
@@ -0,0 +1,8 @@
+package shop.tripn.oracle.mappers;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublisherMapper {
+
+}
---
+++ src/main/java/shop/tripn/oracle/order/domain/OrderDto.java
@@ -0,0 +1,8 @@
+package shop.tripn.oracle.order.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderDto {
+
+}
